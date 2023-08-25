459c1e381af0362da886b1ce53597eec26d28559
Wedam Anewenah <wedamja@gmail.com>
2023-08-25 01:49:09 +0000

    - Replaced type-annotations with type-hints - Removed unneccessary exceptions

--- config/watchtower/scalar_type_definitions/date_time_type_definition.php
+++ config/watchtower/scalar_type_definitions/date_time_type_definition.php
@@ -4,38 +4,27 @@ declare(strict_types=1);
 
 namespace Wedrix\Watchtower\ScalarTypeDefinition\DateTimeTypeDefinition;
 
-use GraphQL\Error\Error;
 use GraphQL\Language\AST\StringValueNode;
-use GraphQL\Utils\Utils;
 
 /**
  * Serializes an internal value to include in a response.
- *
- * @param \DateTime $value
- * @return string
  */
-function serialize($value)
+function serialize(
+    \DateTime $value
+): string
 {
     return $value->format(\DateTime::ATOM);
 }
 
 /**
  * Parses an externally provided value (query variable) to use as an input
- *
- * @param string $value
- * @return \DateTime
  */
-function parseValue($value)
+function parseValue(
+    string $value
+): \DateTime
 {
-    try {
-        return new \DateTime($value);
-    }
-    catch (\Exception $e) {
-        throw new Error(
-            message: "Cannot represent the following value as DateTime: " . Utils::printSafeJson($value),
-            previous: $e
-        );
-    }
+    return \date_create($value) 
+        ?: throw new \Exception('Invalid DateTime value!');
 }
 
 /**
@@ -46,28 +35,12 @@ function parseValue($value)
  *   user(createdAt: "2021-01-24T05:16:41+00:00") 
  * }
  *
- * @param \GraphQL\Language\AST\Node $value
  * @param array<string,mixed>|null $variables
- * @return \DateTime
- * @throws Error
  */
-function parseLiteral($value, ?array $variables = null)
+function parseLiteral(
+    StringValueNode $value, 
+    ?array $variables = null
+): \DateTime
 {
-    if (!$value instanceof StringValueNode) {
-        throw new Error(
-            message: "Query error: Can only parse strings got: $value->kind",
-            nodes: $value
-        );
-    }
-
-    try {
-        return parseValue($value->value);
-    }
-    catch (\Exception $e) {
-        throw new Error(
-            message: "Not a valid DateTime Type", 
-            nodes: $value,
-            previous: $e
-        );
-    }
+    return parseValue($value->value);
 }
